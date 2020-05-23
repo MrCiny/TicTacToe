bf390610fe90ab2570ac6b4b3690dc12a84f9b6e
niks.janeks@gmail.com <niks.janeks@gmail.com>
2020-05-23 18:51:08 +0300

    1.23

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,100 @@ namespace TicTacToe
 {
     public partial class Form1 : Form
     {
+        bool xPlayerTurn = true;
+        int turnCount = 0;
         public Form1()
         {
             InitializeComponent();
+            InitializeGrid();
+            InitializeCells();
+        }
+
+        private void InitializeGrid()
+        {
+            Grid.BackColor = Color.LightCoral;
+            Grid.CellBorderStyle = TableLayoutPanelCellBorderStyle.Inset;
+        }
+
+        private void InitializeCells()
+        {
+            string cellLoop = null;
+            for (int i = 1; i<=9; i++)
+            {
+                cellLoop = "label" + i;
+                Grid.Controls[cellLoop].Text = string.Empty;
+            }
+        }
+
+        private void Player_Click(object sender, EventArgs e)
+        {
+            Label label = (Label)sender;
+
+            if (label.Text != string.Empty){
+              return;
+            }
+
+            //if (label.Text == "")
+            //{
+                if (xPlayerTurn)
+                {
+                    label.Text = "X";
+                }
+                else
+                {
+                    label.Text = "O";
+                }
+            //}
+            turnCount++;
+            CheckForWin();
+            CheckForDraw();
+            xPlayerTurn = !xPlayerTurn;
+        }
+
+        private void CheckForWin()
+        {
+            if ((label1.Text == label2.Text && label2.Text == label3.Text && label1.Text != string.Empty) ||
+                    (label4.Text == label5.Text && label5.Text == label6.Text && label4.Text != string.Empty) ||
+                    (label7.Text == label8.Text && label8.Text == label9.Text && label7.Text != string.Empty) ||
+                    (label1.Text == label4.Text && label4.Text == label7.Text && label1.Text != string.Empty) ||
+                    (label2.Text == label5.Text && label5.Text == label8.Text && label2.Text != string.Empty) ||
+                    (label3.Text == label6.Text && label6.Text == label9.Text && label3.Text != string.Empty) ||
+                    (label1.Text == label5.Text && label5.Text == label9.Text && label1.Text != string.Empty) ||
+                    (label3.Text == label5.Text && label5.Text == label7.Text && label3.Text != string.Empty)
+                )
+            {
+                GameOver();
+            }
+        }
+
+        private void CheckForDraw()
+        {
+            if(turnCount == 9)
+            {
+                MessageBox.Show("Draw!");
+                RestartGame();
+            }
+        }
+
+        private void RestartGame()
+        {
+            InitializeCells();
+            turnCount = 0;
+        }
+        private void GameOver()
+        {
+            string winner;
+            if (xPlayerTurn)
+            {
+                winner = "X";
+            }
+            else
+            {
+                winner = "O";
+            }
+
+            MessageBox.Show(winner + " wins!");
+            RestartGame();
         }
     }
 }
