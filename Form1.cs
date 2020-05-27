2c70fb826c7c395e1da719fc1805df26c59e1d0a
niks.janeks@gmail.com <niks.janeks@gmail.com>
2020-05-27 17:44:41 +0300

    v 1.27

--- Form1.cs
+++ Form1.cs
@@ -16,6 +16,7 @@ namespace TicTacToe
         int turnCount = 0;
         public Form1()
         {
+            this.StartPosition = FormStartPosition.CenterScreen;
             InitializeComponent();
             InitializeGrid();
             InitializeCells();
@@ -34,6 +35,7 @@ namespace TicTacToe
             {
                 cellLoop = "label" + i;
                 Grid.Controls[cellLoop].Text = string.Empty;
+                Grid.Controls[cellLoop].BackColor = Color.LightCoral;
             }
         }
 
@@ -77,6 +79,43 @@ namespace TicTacToe
                 GameOver();
             }
         }
+        private void WinnerCellsChangeColor()
+        {
+            if (label1.Text == label2.Text && label1.Text == label3.Text && label1.Text != "")
+            {
+                //label1.BackColor = label2.BackColor = label3.BackColor = Color.BlueViolet;
+                ResetCellColors(label1, label2, label3, Color.Purple);
+            }
+            else if (label4.Text == label5.Text && label4.Text == label6.Text && label4.Text != "")
+            {
+                ResetCellColors(label4, label5, label6, Color.Purple);
+            }
+            else if (label7.Text == label8.Text && label7.Text == label9.Text && label7.Text != "")
+            {
+                ResetCellColors(label7, label8, label9, Color.Purple);
+            }
+            else if (label1.Text == label4.Text && label1.Text == label7.Text && label1.Text != "")
+            {
+                ResetCellColors(label1, label4, label7, Color.Purple);
+            }
+            else if (label2.Text == label5.Text && label2.Text == label8.Text && label2.Text != "")
+            {
+                ResetCellColors(label2, label5, label8, Color.Purple);
+            }
+            else if (label3.Text == label6.Text && label3.Text == label9.Text && label3.Text != "")
+            {
+                ResetCellColors(label3, label6, label9, Color.Purple);
+            }
+            else if (label1.Text == label5.Text && label1.Text == label9.Text && label1.Text != "")
+            {
+                ResetCellColors(label1, label5, label9, Color.Purple);
+            }
+            else if (label3.Text == label5.Text && label3.Text == label7.Text && label3.Text != "")
+            {
+                ResetCellColors(label3, label5, label7, Color.Purple);
+            }
+
+        }
 
         private void CheckForDraw()
         {
@@ -92,6 +131,13 @@ namespace TicTacToe
             InitializeCells();
             turnCount = 0;
         }
+
+        private void ResetCellColors(Label firstLabel, Label secondLabel, Label thirdLabel, Color color)
+        {
+            firstLabel.BackColor = color;
+            secondLabel.BackColor = color;
+            thirdLabel.BackColor = color;
+        }
         private void GameOver()
         {
             string winner;
@@ -104,7 +150,8 @@ namespace TicTacToe
                 winner = "O";
             }
 
-            MessageBox.Show(winner + " wins!");
+            WinnerCellsChangeColor();
+            MessageBox.Show("Player " + winner + " wins!");
             RestartGame();
         }
     }
